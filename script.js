74ff400fcbe62f94f0357654416a391bc3c2f317
Tommy <TommyCohn116@gmail.com>
2016-12-26 17:34:22 -0500

    Grabbing some html elements.

---
+++ script.js
@@ -0,0 +1,10 @@
+var page = {};
+
+function setup() {
+	page.xInputField = document.getElementById("xFunction");
+	page.yInputField = document.getElementById("yFunction");
+	page.zInputField = document.getElementById("zFunction");
+	page.plotButton = document.getElementById("plotButton");
+}
+
+window.setTimeout(setup, 0);
