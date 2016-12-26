78fc849f7b8feff4690a40b879646e6070646732
Tommy <TommyCohn116@gmail.com>
2016-12-26 17:50:15 -0500

    Return to center button and associated styling and scripting.

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function setup() {
 	page.yInputField = document.getElementById("yFunction");
 	page.zInputField = document.getElementById("zFunction");
 	page.plotButton = document.getElementById("plotButton");
+	page.tminInputField = document.getElementById("tmin");
+	page.tmaxInputField = document.getElementById("tmax");
+	page.tstepInputField = document.getElementById("tstep");
+	page.recenterButton = document.getElementById("returnToCenter");
 }
 
 window.setTimeout(setup, 0);
