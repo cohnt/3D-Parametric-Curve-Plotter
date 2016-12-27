1764b1d5421f7d859e7b49b23de81220066c9cdf
Tommy <TommyCohn116@gmail.com>
2016-12-26 23:02:06 -0500

    Code organization.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-var page = {};
+//Constants
 
+
+//Global Variables
+var page = {};
+var graph;
+
+//Classes
+
+
+//Functions
 function setup() {
 	page.xInputField = document.getElementById("xFunction");
 	page.yInputField = document.getElementById("yFunction");
@@ -12,6 +21,10 @@ function setup() {
 	page.xValid = document.getElementById("xValid");
 	page.yValid = document.getElementById("yValid");
 	page.zValid = document.getElementById("zValid");
+	page.canvas = document.getElementById("graph");
+
+	graph = page.canvas.getContext("2d");
 }
 
+//Executed Code
 window.setTimeout(setup, 0);
